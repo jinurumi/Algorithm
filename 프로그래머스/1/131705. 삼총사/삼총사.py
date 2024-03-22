2401bc14d7737cec23d312f6401910fbc2f16594
jinhye <67266348+jinurumi@users.noreply.github.com>
2024-03-23 03:13:26 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,11 +1,8 @@
 def solution(number):
-    answer = 0
     a=[]
     #number 3총사 구하기
     for i in range(len(number)):
-        #a.append(number[i])
         for j in range(i+1,len(number)):
-            #a.append(number[j])
             for h in range(j+1,len(number)):
                 if number[i] + number[j]+ number[h] ==0:
                     a.append([number[i], number[j],number[h]])
