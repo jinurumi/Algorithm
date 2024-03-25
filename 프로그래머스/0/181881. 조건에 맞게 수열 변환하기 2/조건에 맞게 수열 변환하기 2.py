eeee60942b44e087d64d2d6c8a05eda516906177
jinhye <67266348+jinurumi@users.noreply.github.com>
2024-03-25 18:12:12 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -1,25 +1,61 @@
+import copy
 def solution(arr):
-    count = -1
-    raw = arr.copy()
+    count = 0
+    answer = []
+    raw = arr
     while 1:
-        raw = arr.copy()
-               
+        raw = arr
+        
+        
         for i in range(len(arr)):
             if arr[i] >=50 and arr[i]%2==0:
                 arr[i]=arr[i]//2
             elif arr[i]<=50 and arr[i]%2==1:
                 arr[i]=arr[i]*2+1
 
+        print(arr)
         count +=1
         # 이전 배열과 변환배열이 같다면
         a = 0
+        print(arr)
+        print(raw)
         for i in range(len(arr)):   
             if arr[i] == raw[i]:
                 a +=1
         if len(arr)== a:
+
             return count # 변환횟수 return 
 
+        print(count)
     
     
     
+    
+    
+    
+    
+    
+    
+    
+#     while 1:
+        
+#         arrr= copy.deepcopy(arr)
+        
+#         for i in range(len(arr)):
+#             if arr[i] >= 50 and arr[i]%2==0:
+#                 arr[i] = arr[i]//2
+#             elif arr[i] < 50 and arr[i]%2==1:
+#                 arr[i] = arr[i]*2 +1
+
+#             count +=1
+#         #print(arr)
+        
+#         for i in range(len(arr)):
+#             if arr[i]==arrr[i]:
+#                 answer.append(1)
+#         if sum(answer) == len(arr):
+#             return count
+                
+        
+        
         
